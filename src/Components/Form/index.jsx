60f5584dfefb024811ad5bc13505c374089f0d38
rpruazol <rpruazol@gmail.com>
2023-09-28 22:33:40 -0400

    textarea box conditional rendering

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -5,6 +5,7 @@ import './Form.scss';
 export default function Form(props){
 
   const [url, setUrl] = useState("");
+  const [method, setMethod] = useState(false);
 
 
   const handleSubmit = (e) => {
@@ -15,10 +16,17 @@ export default function Form(props){
     };
     props.handleApiCall(formData);
   }
+
+  const handleMethod = (method) => {
+    setMethod({method: method})
+    console.log(method)
+  }
+  console.log(method)
+
     return (
       <>
         <form onSubmit={handleSubmit} >
-          <select className="methods" name="method">
+          <select className="methods" name="method" onChange={(e) => handleMethod(e.target.value)}>
             <option value="GET" id="get">GET</option>
             <option value="POST" id="post">POST</option>
             <option value="PUT" id="put">PUT</option>
@@ -28,6 +36,9 @@ export default function Form(props){
             <input name='url' type='text' placeholder="https://google.com" onChange={(e) => setUrl(e.target.value)} />
             <button type="submit">GO!</button>
           </label>
+            <textarea name="body" id="" cols="30" rows="10" style={{
+              visibility: ['POST', 'PUT'].includes(method.method) ? "visible" : "hidden" }}>
+              </textarea>
         </form>
       </>
     );
